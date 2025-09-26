c54e7eee6d0e5030e8640a1929c34a7f4b418a87
Kang-dongbae <skylove7890@gmail.com>
2025-09-26 11:04:22 +0900

    modify

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
-print("dd")
-print("dd")
+DATA_DIR = r"C:\Dev\WheelScan\data"
+TRAIN_IMG = DATA_DIR + r"\train\images"
+TRAIN_LABEL = DATA_DIR + r"\train\labels"
 
+MODEL_DIR = r"C:\Dev\WheelScan\models"
